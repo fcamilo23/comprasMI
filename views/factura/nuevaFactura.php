7eafde6ce43e2837b33d147fe137e711104c8f9b
12acastro <88839179+12acastro@users.noreply.github.com>
2022-11-13 10:43:53 -0300

    Detalles de Catelitos

--- views/factura/nuevaFactura.php
+++ views/factura/nuevaFactura.php
@@ -18,16 +18,17 @@
    
 </script>
 <body>
+<nav class="navbar sticky-top navbar-light bg-light" style="z-index:1">
+        <h2 style="color: #001d5a; text-align: center;"class="center">Agregar Factura</h2>
+</nav> 
 <form id="formFactura" onsubmit="validarFormulario(event)" action="<?php echo ROOT_URL; ?>factura/agregarFactura" method="POST" enctype="multipart/form-data">
 
-    <div class="container">
+    <div class="container mt-3 mb-3">
         <div class="row d-flex justify-content-center ">
             <div class="col-sm-12 col-md-12 col-lg-12 col-xl-10 col-xxl-8">
                 <div class="card">
-                    <br>
-                    <h1 style="color: #001d5a; text-align:center; margin-bottom: 10px" class="center">Agregar Factura</h1>
-
                         <div class="card-body ">
+                        <h4 style="color: #001d5a; margin-left: 25px" class="">Datos de la Orden:</h4>
 
                             <input type="hidden" name="idOrden" value="<?php echo $viewmodel['orden']['id']; ?>">
                             <input type="hidden" name="idProveedor" value="<?php echo $viewmodel['idProveedor']; ?>">
@@ -53,7 +54,7 @@
                                         ?>
                         
                             
-                                <h5><b>DATOS DE LA ORDEN:</b></h5>
+
                                 <div class="card">
                                     <div class="card-body">
                                         <h6><b>PROCEDIMIENTO: </b> <?php echo $viewmodel['orden']["procedimiento"]?></h6>
@@ -72,7 +73,7 @@
                                     
                                     <div class="card-body">
                                         <h4 style="color: #001d5a; margin-left: 25px" class="">Subir Archivos<b class="text-danger">*</b></h4>
-    
+                                        <hr>
                                         <div class="mb-3">
                                         <input class="form-control" id="loadFile" accept="application/pdf" type="file" onchange="readAsBase64()"  width="190px" height="50px"/>
                                         </div>
@@ -87,6 +88,8 @@
 
                                 <div class="card" style="margin-top: 10px;">
                                     <div class="card-body">
+                                        <h4 style="color: #001d5a; margin-left: 25px" class="">Datos:</h4>
+                                        <hr>
                                         <div class="mb-3 row">
                                             <label for="numeroFactura" class="col-sm-2 col-form-label"> N° Factura<b class="text-danger">*</b></label>
                                             <div class="col-sm-6">
@@ -161,8 +164,9 @@
         <div class="col-sm-12 col-md-12 col-lg-12 col-xl-10 col-xxl-10" >
             <div class="card text-center" style="margin-top: 40px; margin-bottom: 50px;">                
                 <div class="card-body ">
+            
+            <h4 style="color: #001d5a; margin-left: 25px" class="">Items:</h4>
             <hr>
-            <h3 style="color: #001d5a; margin-left: 25px" class="">Items</h3>
                         <div id="main-container" style="width: 100%; overflow: auto; padding: 15px; max-height: 800px">
 
                                 <table id="listaItems" style="width: 100%;">
